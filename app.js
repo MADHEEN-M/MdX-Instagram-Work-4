3326bc843da91bc5e51b63b31659e031313a28cb
MADHEEN-M <madheen2009@gmail.com>
2026-07-09 14:29:55 +0530

    MDX-Official Work

--- app.js
+++ app.js
@@ -7,6 +7,70 @@ import {
     increment
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+
+
+const data = [
+    {
+        "title": "Echo-Ads Free Music Listen",
+        "image": "images/echo.png",
+        "downloadUrl": "https://drive.google.com/file/d/1BD2eDLj888DI32r8pUt1oYp9ZPHh-sYZ/view?usp=drivesdk"
+    },
+    {
+        "title": "GTA-5 Game Android Edition",
+        "image": "images/Gta-5.jpg",
+        "downloadUrl": "https://drive.google.com/file/d/1KplTYgrpK99dSu2Y-x7OzIcPhk8OR_Un/view?usp=drivesdk"
+    },
+    {
+        "title": "Capcut Pro-2026",
+        "image": "images/capcut app.jpg",
+        "downloadUrl": "https://drive.google.com/file/d/1DElZXMHgP-ia_EpmWZaBxepBsxRqtmji/view?usp=drivesdk"
+    },
+    {
+        "title": "Proton VPN Pro-2026",
+        "image": "images/vpn.jpg",
+        "downloadUrl": "https://t.me/madxedits20/126"
+    },
+    {
+        "title": "Alight Motion Pro Mod-2026",
+        "image": "images/1alightmotion.png",
+        "downloadUrl": "https://t.me/madxedits20/128"
+    },
+    {
+        "title": "Horizon Driving Ultra-Mod",
+        "image": "images/HorizonDrivingSimulator.png",
+        "downloadUrl": "https://drive.google.com/file/d/1v7SUzijiqA5jsTaYek7nrf1IN_yp4yTH/view?usp=drivesdk"
+    },
+    {
+        "title": "LightRoom Premieum-Mod Apk",
+        "image": "images/LR.jpg",
+        "downloadUrl": "https://t.me/madxedits20/127"
+    },
+    {
+        "title": "Open Camera Apk",
+        "image": "images/open camera.jpg",
+        "downloadUrl": "https://play.google.com/store/apps/details?id=net.sourceforge.opencamera"
+    },
+
+]
+
+const container = document.getElementById('appContainer')
+
+data.map((e, i) => {
+
+   container.innerHTML += `
+<div class="card">
+    <img src="${e.image}" width="600" height="300" alt="Upload App Image">
+    <div class="card-content">
+        <h2>${e.title}</h2>
+
+        <a href="javascript:void(0)" class="btn" onclick="countDownload('${e.downloadUrl}')">
+            Download
+        </a>
+    </div>
+</div>
+`;
+})
+
 const firebaseConfig = {
     apiKey: "AIzaSyDT-_43lioLSjZXTNlDDLKYzxXMJw3q3sk",
     authDomain: "whotfx-madx.firebaseapp.com",
@@ -22,20 +86,21 @@ const db = getFirestore(app);
 console.log("Firebase Connected!");
 
 
-document.getElementById("searchInput").addEventListener("keyup", function () {
+const searchInput = document.getElementById("searchInput");
 
-    let search = this.value.toLowerCase();
+searchInput.addEventListener("input", function () {
 
-    document.querySelectorAll(".card").forEach(card => {
+    const search = this.value.toLowerCase().trim();
 
-        let name = card.querySelector("h2").textContent.toLowerCase();
+    document.querySelectorAll(".card").forEach((card) => {
 
-        card.style.display = name.includes(search) ? "" : "none";
+        const appName = card.querySelector("h2").textContent.toLowerCase();
+
+        card.style.display = appName.includes(search) ? "" : "none";
 
     });
 
 });
-
 
 window.enterSite = function () {
 
@@ -91,3 +156,53 @@ window.countDownload = async function (link) {
     window.open(link, "_blank");
 
 };
+
+
+const followBtn = document.getElementById("followbtnId");
+const youtubeBtn = document.getElementById("youtubeBtnId");
+const enterBtn = document.getElementById("enterbtnId");
+const followPopup = document.getElementById("followPopup");
+
+enterBtn.style.display = "none";
+
+function updateUI() {
+
+    const youtubeDone = localStorage.getItem("youtube") === "1";
+    const instagramDone = localStorage.getItem("flag") === "1";
+
+    if (youtubeDone && instagramDone) {
+        enterBtn.style.display = "inline-block";
+    } else {
+        enterBtn.style.display = "none";
+    }
+}
+
+updateUI();
+
+window.addEventListener("pageshow", updateUI);
+window.addEventListener("focus", updateUI);
+
+youtubeBtn.addEventListener("click", () => {
+
+    localStorage.setItem("youtube", "1");
+
+    window.open("https://youtube.com/@madzpov?si=EC2vVLfZUcjlFe9I", "_blank");
+
+});
+
+followBtn.addEventListener("click", () => {
+
+    localStorage.setItem("flag", "1");
+
+    window.location.href = "https://www.instagram.com/whotfx_madx?igsh=MWdrN2dreTl4aGE3MQ==";
+
+});
+
+enterBtn.addEventListener("click", () => {
+
+    followPopup.style.display = "none";
+
+    localStorage.removeItem("youtube");
+    localStorage.removeItem("flag");
+
+});
